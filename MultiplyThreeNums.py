88921ef1d456747f8727b09ef3a79d42ca4d6fce
Whelve-Autism <2303085802@qq.com>
2025-03-21 21:41:59 +0800

    MultiplyThreeNums.py to repository

---
+++ MultiplyThreeNums.py
@@ -0,0 +1,5 @@
+num1 = float(input("Please enter the first number: "))
+num2 = float(input("Please enter the second number: "))
+num3 = float(input("Please enter the third number: "))
+result = num1 * num2 * num3
+print("The answer is: " + str(result))
